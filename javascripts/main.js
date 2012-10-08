042a7afbaae9b6a88f2a2862dfb3b3d63d42f02c
Jean-Bernard Ratté <jean.bernard.ratte@unary.ca>
2012-10-08 14:18:12 -0400

    Modified main.js to use jQuery over POJS

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,23 @@
-function quote(json) {
-	if (json.body != null) {
-		document.getElementById('body').innerHTML = json.body;
-		document.getElementById('author').innerHTML = json.author;
-		document.getElementById('date').innerHTML = json.date;
-	} else {
-		document.getElementById('quote-container').innerHTML = '';
-	}
+//function quote(json) {
+//	if (json.body != null) {
+//		document.getElementById('body').innerHTML = json.body;
+//		document.getElementById('author').innerHTML = json.author;
+//		document.getElementById('date').innerHTML = json.date;
+//	} else {
+//		document.getElementById('quote-container').innerHTML = '';
+//	}
+//}
+
+$(document).ready(function() {
+	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+		function(data) {
+			if (data.body != null) {
+	  			$('#body').innerHTML = data.body;
+	  			$('#author').innerHTML = data.author;
+	  			$('#date').innerHTML = data.date;
+			} else {
+	  			$('#quote-container').innerHTML = '';
+			}
+		}
+	);
 }
