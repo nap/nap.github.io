26050b9346aaf9c9d45dc9e4e0befdf8ce45404c
Jean-Bernard Ratté <jean.bernard.ratte@unary.ca>
2012-10-08 14:50:06 -0400

    JS and HTML tweeks.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,23 +1,8 @@
-//function quote(json) {
-//	if (json.body != null) {
-//		document.getElementById('body').innerHTML = json.body;
-//		document.getElementById('author').innerHTML = json.author;
-//		document.getElementById('date').innerHTML = json.date;
-//	} else {
-//		document.getElementById('quote-container').innerHTML = '';
-//	}
-//}
-
-$(document).ready(function() {
-	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-		function(data) {
-			if (data.body != null) {
-	  			$('#body').innerHTML = data.body;
-	  			$('#author').innerHTML = data.author;
-	  			$('#date').innerHTML = data.date;
-			} else {
-	  			$('#quote-container').innerHTML = '';
-			}
-		}
-	);
+function quote(data) {
+	if (data.body != null) {
+		$('#quote-container').toggle();
+		$('#body').html(data.body);
+		$('#author').html(data.author);
+		$('#date').html(data.date);
+	}
 }
