7a976d54a3d149a4c08da0082f5ff205a7cac8f7
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2017-03-05 18:07:37 -0500

    ignore ide project file

--- javascripts/main.js
+++
@@ -1,8 +0,0 @@
-function quote(data) {
-	if (data.body != null) {
-		$('#quote-container').toggle();
-		$('#body').html(data.body);
-		$('#author').html(data.author);
-		$('#date').html(data.date);
-	}
-}
